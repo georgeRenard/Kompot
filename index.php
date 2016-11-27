92505205728a89d2f60d46d207772c0b28bfcb94
Dimitrow <dimitrow00@gmail.com>
2016-11-27 20:28:25 +0200

    Index page layout.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 ?>
     <!DOCTYPE html>
-    <html>
+<html xmlns:style="http://www.w3.org/1999/xhtml">
 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -13,10 +13,11 @@
         <meta name="google" content="notranslate" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Coming Soon</title>
+        <title>KOR</title>
 
         <link type="text/css" rel="stylesheet" href="Blog/css/bootstrap.css">
         <link type="text/css" rel="stylesheet" href="Blog/css/page.css">
+        <link type="text/css" rel="stylesheet" href="Blog/css/index.css">
         
         
         <script type="text/jscript" href="Blog/scripts/bootstrap.js"></script>
@@ -28,16 +29,47 @@
     <body>
 
         <header>
+            <div class="container-fluid">
+                <div class="row" align="right">
+                    <div class="col-md-12">
+                        <div class="btn-toolbar" >
+                            <button class="button" onclick="location.href='Blog/web/login.php'" type="button">Sign Up
+                            </button>
+                            <button class="button" onclick="location.href='Blog/web/register.php'" type="button">Register
+                            </button>
+                            <button class="button" onclick="location.href='#'" type="button">About Us
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </header>
         <main>
-            <a href="Blog/web/register.php">Register</a>
-            <br>
-            <a href="Blog/web/login.php">Log In</a>
+            <div class="main">
+                <div class="container-main">
+                    <h1>True Slavs ||| Present:</h1>
+                    <h1 class="main-text">Kompot OF Rhytms</h1>
+                    <h2 class="subtitle">Life is too short not to share music!</h2>
+                    <div class="center">
+                        <div class="inner-center">
+                            <div class="btn-toolbar" >
+                                <button class="button" onclick="location.href='#'" type="button">Listen
+                                </button>
+                                <button class="button" onclick="location.href='Blog/web/login.php'" type="button">Upload
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </main>
         
         <footer>
-            <p>We do not own any of the rights regarding the music we post. It's only for entertainment purposes! Joro L,Paco K, Jujo P</p>
+            <div class="footer">
+                <div class="container-foot">
+                    <h1>We do not own any of the rights regarding the music we post. It's only for entertainment purposes! True Slavs |||</h1>
+                </div>
+            </div>
         </footer>
         
     </body>
